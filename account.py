8b3c1c6f2f9c026c4b3410aa6215be7d6f658db5
CosBett <cosmasbett9@gmail.com>
2022-01-23 20:06:29 +0300

    update user accounts class

--- account.py
+++ account.py
@@ -7,7 +7,7 @@ class Account:
     def __init__(self, first_name, last_name, username, password):  # New instances and variables
         self.first_name = first_name
         self.last_name = last_name
-        self.user_name = username
+        self.username = username
         self.password = password
 
     def save_account(self):
@@ -27,8 +27,18 @@ class Account:
     @classmethod
     def find_by_username(cls, username):
         '''
-       This function allow a user to search account by username and return back the account
-       '''
+        This function allow a user to search account by username and return back the account
+        '''
         for account in cls.accounts_list:
-            if account.account_list == username:
+            if account.username == username:
                 return account
+
+    @classmethod
+    def account_exists(cls, username):
+        '''
+        This function checks if the username exist in the stored list of accounts
+        '''
+        for account in cls.accounts_list:
+            if account.username == username:
+                return True
+        return False
--- account_test.py
+++ account_test.py
@@ -1,4 +1,3 @@
-from cgi import test
 import unittest  # Importing the unittest module
 from account import Account
 
@@ -6,7 +5,7 @@ from account import Account
 class TestAccount(unittest.TestCase):
 
     '''
-    Test class that defines test cases for the account class behaviours.
+    Test class that defines test cases for the account class behaviours
 
     Args:
         unittest.TestCase: TestCase class that helps in creating test cases
@@ -14,10 +13,10 @@ class TestAccount(unittest.TestCase):
 
     def setUp(self):
         '''
-        Set up method to run before each test cases.
+        Set up method to run before each test cases
         '''
         self.new_account = Account(
-            "Cosmas", "Bett", "Prime", "M@2021")  # create Account object
+            "Cosmas", "Bett", "Prime", "M@2021")  # Account object
 
     def test_init(self):
         '''
@@ -26,12 +25,12 @@ class TestAccount(unittest.TestCase):
 
         self.assertEqual(self.new_account.first_name, "Cosmas")
         self.assertEqual(self.new_account.last_name, "Bett")
-        self.assertEqual(self.new_account.user_name, "Prime")
+        self.assertEqual(self.new_account.username, "Prime")
         self.assertEqual(self.new_account.password, "M@2021")
 
     def tearDown(self):
         '''
-        tearDown method that does clean up after each test case has run.
+        tearDown method that does clean up after each test case has run
         '''
         Account.accounts_list = []
 
@@ -69,6 +68,32 @@ class TestAccount(unittest.TestCase):
 
         self.new_account.delete_account()
         self.assertEqual(len(Account.accounts_list), 1)
+
+    def test_find_account_by_username(self):
+        '''
+        This test checks if we can find an account by username and display information
+        '''
+        self.new_account.save_account()
+        test_account = Account(
+            "Cosmas", "Bett", "Prime", "M@2021")
+
+        test_account.save_account()
+
+        found_account = Account.find_by_username('Prime')
+        self.assertEqual(found_account.username, test_account.username)
+
+    def test_account_exists(self):
+        '''
+        Test to check if the account exist through the accounts list
+        '''
+        self.new_account.save_account()
+        test_account = Account(
+            "Cosmas", "Bett", "Prime", "M@2021")
+
+        test_account.save_account()
+
+        account_exists = Account.account_exists('Prime')
+        self.assertTrue(account_exists)
 
 
 if __name__ == '__main__':
