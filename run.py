b96f0cd01e91312143dfe6b427a93cb6c6fc9bce
CosBett <cosmasbett9@gmail.com>
2022-01-24 14:01:33 +0300

    updated with docstrings

--- run.py
+++ run.py
@@ -9,20 +9,35 @@ def create_account(first_name, last_name, username, password):
 
 
 def save_account(account):
+    ''' 
+    Function to save contact
+    '''
     account.save_account()
 
 
 def delete_account(account):
+    ''' 
+    Function to delete contact
+    '''
     account.delete_account()
 
 
 def find_account(username):
+    ''' 
+    Function to find contact by username
+    '''
     return Account.find_by_username(username)
 
 
 def exist_account(username):
+    ''' 
+    Function to show contact exist
+    '''
     return Account.account_exists(username)
 
 
 def display_account():
+    ''' 
+    Function to display contact 
+    '''
     return Account.display_account()
