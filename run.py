d406ac33aa9e89f5aacd86a57d8aadcd9a3f3a58
CosBett <cosmasbett9@gmail.com>
2022-01-24 13:51:36 +0300

    create a method for account creation

--- run.py
+++ run.py
@@ -1,3 +1,8 @@
 #!/usr/bin/env python3.9
 from account import Account
 from password import Password
+
+
+def create_account(first_name, last_name, username, password):
+    account = Account(first_name, last_name, username, password)
+    return account
