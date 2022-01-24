68ee4c6dbd45904117cdb9af502ad41e1b4df687
CosBett <cosmasbett9@gmail.com>
2022-01-24 14:07:23 +0300

    create save,delete and display function for password class in run.py

--- run.py
+++ run.py
@@ -2,6 +2,8 @@
 from account import Account
 from password import Password
 
+# Account class
+
 
 def create_account(first_name, last_name, username, password):
     account = Account(first_name, last_name, username, password)
@@ -41,3 +43,22 @@ def display_account():
     Function to display contact 
     '''
     return Account.display_account()
+
+# Password Class
+
+
+def create_site(site, password):
+    passwords = Password(site, password)
+    return passwords
+
+
+def save_site(passwords):
+    passwords.save_site()
+
+
+def delete_site(passwords):
+    passwords.delete_site()
+
+
+def display_sites():
+    return Password.display_sites()
