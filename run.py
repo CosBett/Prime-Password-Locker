7a17b3f314e3e3756c4491ddaf451bcbf84e8829
CosBett <cosmasbett9@gmail.com>
2022-01-25 05:18:04 +0300

    update main function

--- run.py
+++ run.py
@@ -12,36 +12,36 @@ def create_account(first_name, last_name, username, password):
 
 
 def save_account(account):
-    ''' 
+    '''
     Function to save contact
     '''
     account.save_account()
 
 
 def delete_account(account):
-    ''' 
+    '''
     Function to delete contact
     '''
     account.delete_account()
 
 
 def find_account(username):
-    ''' 
+    '''
     Function to find contact by username
     '''
     return Account.find_by_username(username)
 
 
-def exist_account(username):
-    ''' 
+def isexits_account(username):
+    '''
     Function to show contact exist
     '''
     return Account.account_exists(username)
 
 
 def display_account():
-    ''' 
-    Function to display contact 
+    '''
+    Function to display contact
     '''
     return Account.display_account()
 
@@ -61,6 +61,14 @@ def delete_site(passwords):
     passwords.delete_site()
 
 
+def find_site(page):
+    return Password.find_by_site(page)
+
+
+def isexist_site(page):
+    return Password.site_exists(page)
+
+
 def display_sites():
     return Password.display_sites()
 
@@ -77,7 +85,14 @@ def main():
         print('1) Display Accounts \n 2 Login \n 3) Create Account \n 4) Exit')
 
         option = int(input())
-        if option == 2:
+        if option == 1:
+            if display_account():
+                for account in display_account():
+                    print(f'{account.username}')
+            else:
+                print('No Accounts Found')
+
+        elif option == 2:
             print('Enter your username')
             username = input()
             print('Énter your password')
@@ -92,8 +107,8 @@ def main():
 
                     print(
                         '1) Save new Password \n 2) Display Saved Password \n 3) Delete Password \n 4) Exit')
-                    log_option = int(input())
-                    if log_option == 1:
+                    login_option = int(input())
+                    if login_option == 1:
                         print('New site')
                         print('˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜')
 
@@ -104,7 +119,87 @@ def main():
                         password = input()
 
                         save_site(create_site(site, password))
-                    elif log_option == 2:
+                    elif login_option == 2:
                         if display_sites():
                             for page in display_sites():
                                 print(f'{page.site}:{page.paseword}')
+                        else:
+                            print('No password saved for this site!')
+                            print('\n')
+
+                    elif login_option == 3:
+                        print('Please enter the name of site to delete')
+
+                        page = input()
+                        if isexist_site(site):
+                            remove_site = (site)
+                            delete_site(remove_site)
+
+                        else:
+                            print(
+                                f'Above {site} is not found, Enter a saved site to delete')
+                    elif login_option == 4:
+                        print('Thank you, Bye')
+                        break
+            else:
+                print('Incorrect Username or password!')
+
+        elif option == 3:
+            print('New Account')
+            print('˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜')
+
+            print('Enter your First Name')
+            first_name = input()
+
+            print('Enter your Last_Name')
+            last_name = input()
+
+            print('Enter Username')
+            username = input()
+
+            print('Enter your Password')
+            password = input()
+
+            save_account(create_account(
+                first_name, last_name, username, password))
+            print('Account has been created!')
+            while True:
+                print(
+                    f'Welcome {username} Kindly reply with below Numbers for an Option')
+
+                print(
+                    '1) Save new Password \n 2) Display Saved Password \n 3) Delete Password \n 4) Exit')
+                login_option = int(input())
+                if login_option == 1:
+                    print('New site')
+                    print('˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜')
+
+                    print('Enter Site Name')
+                    site = input()
+
+                    print('Enter password')
+                    password = input()
+
+                    save_site(create_site(site, password))
+                elif login_option == 2:
+                    if display_sites():
+                        for page in display_sites():
+                            print(f'{page.site}:{page.paseword}')
+                    else:
+                        print('No password saved for this site!')
+                        print('\n')
+
+                elif login_option == 3:
+                    print('Please enter the name of site to delete')
+
+                    page = input()
+                    if isexist_site(site):
+                        remove_site = (site)
+                        delete_site(remove_site)
+
+                    else:
+                        print(
+                            f'Above {site} is not found, Enter a saved site to delete')
+                elif login_option == 4:
+                    print('Thank you, Bye')
+                    break
