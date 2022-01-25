c180d5cf028099464fc0130519c6b0349019838a
CosBett <cosmasbett9@gmail.com>
2022-01-25 04:30:10 +0300

    create log on option for the main function

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3.9
+from ast import While
 from account import Account
 from password import Password
 
@@ -62,3 +63,48 @@ def delete_site(passwords):
 
 def display_sites():
     return Password.display_sites()
+
+ # Main function
+
+
+def main():
+    print('Welcome  To Prime Password Locker')
+    print('/˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜')
+    print('Kindly reply with below Numbers for an Option')
+
+    while True:
+
+        print('1) Display Accounts \n 2 Login \n 3) Create Account \n 4) Exit')
+
+        option = int(input())
+        if option == 2:
+            print('Enter your username')
+            username = input()
+            print('Énter your password')
+            password = input()
+            account = find_account(username)
+            if account.username == username and account.password == password:
+
+                print('Logged in Successfully')
+                while True:
+                    print(
+                        f'Welcome {username}, Kindly reply with below Numbers for an Option')
+
+                    print(
+                        '1) Save new Password \n 2) Display Saved Password \n 3) Delete Password \n 4) Exit')
+                    log_option = int(input())
+                    if log_option == 1:
+                        print('New site')
+                        print('˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜')
+
+                        print('Enter Site Name')
+                        site = input()
+
+                        print('Enter password')
+                        password = input()
+
+                        save_site(create_site(site, password))
+                    elif log_option == 2:
+                        if display_sites():
+                            for page in display_sites():
+                                print(f'{page.site}:{page.paseword}')
